ed7a81ec476c72bb0752a922326a792e256decd5
dertder25t-png <dertder25t@gmail.com>
2025-11-14 06:12:39 +0000

    chore(db): add migration for Jarvis and ActivitySignals tables

--- src/FocusDeck.Persistence/Migrations/20251114061155_AddJarvisAndActivitySignals.Designer.cs
+++ src/FocusDeck.Persistence/Migrations/20251114061155_AddJarvisAndActivitySignals.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FocusDeck.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FocusDeck.Persistence.Migrations
 {
     [DbContext(typeof(AutomationDbContext))]
-    partial class AutomationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251114061155_AddJarvisAndActivitySignals")]
+    partial class AddJarvisAndActivitySignals
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
